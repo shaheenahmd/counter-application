38e7963c83c4abda758001e2c9c2f2890b3e57aa
shaheenahmd <shaheenahmdk@gmail.com>
2022-12-13 23:52:24 +0530

    first commmit

---
+++ main.js
@@ -0,0 +1,37 @@
+let count=0;
+
+var incriment=document.querySelector("#incri");
+var dicriment=document.querySelector("#dicri");
+var reset= document.querySelector("#reset");
+var result= document.querySelector("#result");
+
+
+
+function incrim(){
+    count=count+1;
+    result.innerHTML=count;
+    if(count>0){
+        result.style.color='green'
+    }
+    if(count==0){
+        result.style.color='black'
+    }
+
+} 
+
+function dicrim(){
+    count=count-1;
+    result.innerHTML=count;
+    if(count<0){
+        result.style.color='red'
+    }
+    if(count==0){
+        result.style.color='black'
+    }
+   
+} 
+function resetvalue(){
+    count=0;
+    result.innerHTML=count;
+    result.style.color='black'
+} 
